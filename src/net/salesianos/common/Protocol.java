b94b24727c38043cc1c1c0104e218321e83662cf
ori05paz <159430429+ori05paz@users.noreply.github.com>
2025-02-11 22:21:37 +0000

    feat(Protocol): Defined communication protocols

---
+++ src/net/salesianos/common/Protocol.java
@@ -0,0 +1,12 @@
+package net.salesianos.common;
+
+/**
+ * Defines communication protocols used between the client and server.
+ * Contains predefined message types for various operations.
+ */
+public class Protocol {
+    public static final String ADD_EVENT = "ADD";
+    public static final String REMOVE_EVENT = "REMOVE";
+    public static final String EVENT_UPDATE = "EVENT_UPDATE";
+    public static final String LOGIN = "LOGIN";
+}
